57e6457e2e5549ded8fd82f0026e17fab96d6c60
shuyao <shuyao.guo@tu-berlin.de>
2023-07-21 14:34:50 +0200

    Added import global_settings for external use

--- busmodel/settings.py
+++ busmodel/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+# Only uncomment when used as external dependency
+# from django.conf.global_settings import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
