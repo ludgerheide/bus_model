ee236341af322793dfbd31d434d77eec5ae5fcab
shuyao <shuyao.guo@tu-berlin.de>
2023-07-21 13:53:21 +0200

    Added VehicleType django model according to miro board.

--- busmodel/models.py
+++ busmodel/models.py
@@ -1,11 +1,32 @@
 from django.db import models
 
 
-class LudgersVehicle(models.Model):
-    name = models.CharField(max_length=255)
-    dummy_field_1 = models.IntegerField()
-    dummy_field_2 = models.DateTimeField()
-    dummy_field_3 = models.CharField(max_length=255)
+class VehicleType(models.Model):
+    """ Django model class representing a vehicle type
+
+    Attributes:
+    vehicle_type_id: [int] id for each type as primary key
+    vehicle_type_name: [str] name of each type
+    effective_capacity: [str] effective of battery capacity (kWh)
+    opportunity_charging_possible: [bool] True for possible for opportunity charging
+    effective_charging_curve: [str]
+    effective_v2g_curve: [str]
+    charging_efficiency: [float]
+    minimal_charging_power: [float] in kW
+    constant_evergy_consumption: [float] energy consumption when bus stops. can be 0
+    vehicle_length: [float] in meters, for optimization of rotation plan
+    """
+    vehicle_type_id = models.IntegerField(primary_key=True)
+    vehicle_type_name = models.CharField(max_length=255)
+    effective_capacity = models.FloatField()
+    opportunity_charging_possible = models.BooleanField()
+    effective_charging_curve = models.CharField(max_length=255)
+    effective_v2g_curve = models.CharField(max_length=255)
+    charging_efficiency = models.FloatField()
+    minimal_charging_power = models.FloatField()
+    constant_evergy_consumption = models.FloatField()
+    vehicle_length = models.FloatField()
 
     def __str__(self):
-        return f"<Vehicle name=\"{self.name}\">"
+        """for test only"""
+        return f'{self.vehicle_type_name}, {self.vehicle_name}'
